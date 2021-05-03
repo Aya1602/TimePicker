b4de883ea67ab08025f34d06bed9aad408e3e2a6
Aya1602 <aygerim.m1602@gmail.com>
2021-05-03 14:21:58 +0600

    Initial commit

---
+++ app/src/main/java/com/example/timepicker/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.timepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView timeTextView = (TextView)findViewById(R.id.timeTextView);
+        TimePicker timePicker = (TimePicker)this.findViewById(R.id.timePicker);
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+
+                timeTextView.setText("Время: " + hourOfDay + ":" + minute);
+                // или так
+                // timeTextView.setText("Время: " + view.getHour() + ":" + view.getMinute());
+            }
+        });
+    }
+}
